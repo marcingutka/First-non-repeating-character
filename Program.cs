a24de3cf8ee937cb396f864ffcbcbe3c3f950144
macingutka <gucios17@gmail.com>
2019-07-30 10:33:58 +0200

    first LINQ commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace First_non_repeating_character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstNonRepeatingLetter(""));
+            Console.WriteLine(FirstNonRepeatingLetter("Hello World"));
+            Console.WriteLine(FirstNonRepeatingLetter("a"));
+            Console.WriteLine(FirstNonRepeatingLetter("stress"));
+        }
+
+        public static string FirstNonRepeatingLetter(string s)
+        {                       
+            return new string(s.Select(c => s.Count(x => c.ToString().ToLower() == x.ToString().ToLower()) == 1 ? c.ToString() : "").ToArray().FirstOrDefault(q => q != "")) ?? "";
+        }
+    }
+}
