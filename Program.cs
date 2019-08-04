95f8daa927cb7cc641bc77e953bb40706aa59275
macingutka <gucios17@gmail.com>
2019-08-04 10:47:23 +0200

    New solution - ToString()

--- Program.cs
+++ Program.cs
@@ -7,15 +7,19 @@ namespace First_non_repeating_character
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(default(string));
             Console.WriteLine(FirstNonRepeatingLetter(""));
             Console.WriteLine(FirstNonRepeatingLetter("Hello World"));
             Console.WriteLine(FirstNonRepeatingLetter("a"));
             Console.WriteLine(FirstNonRepeatingLetter("stress"));
+            Console.WriteLine(FirstNonRepeatingLetter("seeess"));
         }
 
         public static string FirstNonRepeatingLetter(string s)
-        {                       
-            return new string(s.Select(c => s.Count(x => c.ToString().ToLower() == x.ToString().ToLower()) == 1 ? c.ToString() : "").ToArray().FirstOrDefault(q => q != "")) ?? "";
+        {
+            string result = (s.FirstOrDefault(c => s.Count(x => c.ToString().ToLower() == x.ToString().ToLower()) == 1).ToString());
+            return result != default(string) ? result : string.Empty;
         }
     }
 }
+
